57faa5a214b500b5d0984fffd738954b86ddeb48
ingsoft3ucc <140459109+ingsoft3ucc@users.noreply.github.com>
2023-09-26 23:06:52 -0300

    Add files via upload
    init

---
+++ MyModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Post
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
+
+// public class Root
+// {
+//     public List<Post> Posts { get; set; }
+// }
